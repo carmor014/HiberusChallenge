483e1c9bd4ca8d40172a09781707ed95d020df05
Carmor014 <carmor_014@hotmail.com>
2020-06-27 14:52:23 -0500

    add order entity

--- checkout/src/main/java/com/hiberus/checkout/models/entitys/Order.java
+++ checkout/src/main/java/com/hiberus/checkout/models/entitys/Order.java
@@ -2,15 +2,57 @@ package com.hiberus.checkout.models.entitys;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 public class Order implements Serializable {
 	
+	
+	
+
 	private Long id;
+	
 	private Date date;
-	private String direccion;
 	
+	private String direction;
 	
+	private  List<Product> products;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
 
 }
